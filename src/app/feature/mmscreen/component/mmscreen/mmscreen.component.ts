59e70b8c2e9a129af3160c680d4baddb3a390e0e
deejayy <deejayy@deejayy.hu>
2020-07-07 00:12:48 +0200

    Adjust sitebuild

--- src/app/feature/mmscreen/component/mmscreen/mmscreen.component.ts
+++ src/app/feature/mmscreen/component/mmscreen/mmscreen.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mmscreen.component.scss'],
 })
 export class MmscreenComponent implements OnInit {
-  public showModal: boolean = true;
+  public showModal: boolean = false;
 
   constructor() {}
 
