3f19fe812377f2ba39c42b44f6746bd8c5b14dea
deejayy <deejayy@deejayy.hu>
2020-07-06 23:50:47 +0200

    Add modal sitebuild

--- src/app/feature/mmscreen/component/mmscreen/mmscreen.component.ts
+++ src/app/feature/mmscreen/component/mmscreen/mmscreen.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mmscreen.component.scss'],
 })
 export class MmscreenComponent implements OnInit {
+  public showModal: boolean = true;
+
   constructor() {}
 
   public ngOnInit() {}
