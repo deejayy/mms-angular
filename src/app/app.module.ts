4419564722b851f1e784b8d6df5c03b504b18d18
deejayy <deejayy@deejayy.hu>
2020-07-08 00:04:01 +0200

    Wire in core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppFrameComponent } from './shared/app-frame/app-frame.component';
+import { CoreModule } from './core/core.module';
+
+import * as authStore from './core/auth/store/auth-meta.reducer';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent, AppFrameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    StoreModule.forRoot({} as any, { metaReducers: [authStore.initStateFromLocalStorage] }),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
